420835809380dcb44be83ff54c6eaa30c571ccb5
David <Davidmaxsands@gmail.com>
2022-02-09 10:18:47 -0700

    completed advanced

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,10 @@ function App() {
   const [listings, setListings] = useState([])
   const [filterListing, setFilterListing] = useState([])
   const [search, setSearch] = useState('')
+  const [checked, setChecked] = useState(false)
+  const [newDescription, setNewDescription] = useState('')
+  const [newImage, setNewImage] = useState('')
+  const [newLocation, setNewLocation] = useState('')
   const listing_url = 'http://localhost:6001/listings'
 
   useEffect(() => {
@@ -17,6 +21,10 @@ function App() {
     })
   }, [])
 
+  function handleClick() {
+    setChecked(checked => !checked)
+  }
+
   function deleteListing(id) {
     const updatedListing = filterListing.filter(listing => listing.id !== id)
     setFilterListing(updatedListing)
@@ -27,10 +35,61 @@ function App() {
     .then(data => console.log(data))
   }
 
+  useEffect(() => {
+    let sortedArray = []
+    if(checked) {
+      function compare(a, b) {
+        if(a.location < b.location){
+        return -1
+        }
+        if(a.location > b.location) {
+        return 1
+        }
+        return 0
+      }
+      sortedArray = [...filterListing].sort(compare)
+    } else {
+      sortedArray = listings
+    }
+    setFilterListing(sortedArray)
+  }, [checked])
+
+  function handleFormSubmit(e) {
+    e.preventDefault()
+    const newObj = {
+      description: newDescription,
+      image: newImage,
+      location: newLocation
+    }
+    const updatedListings = [...filterListing, newObj]
+    setFilterListing(updatedListings)
+    fetch(listing_url, {
+      method: 'POST', 
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newObj)
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      })
+  }
+
   return (
     <div className="app">
       <Header search={search} setSearch={setSearch} setFilterListing={setFilterListing} listings={listings} />
-      <ListingsContainer listings={filterListing} deleteListing={deleteListing} />
+      <ListingsContainer 
+      listings={filterListing} 
+      deleteListing={deleteListing} 
+      checked={checked} 
+      handleClick={handleClick}
+      newDescription={newDescription}
+      newImage={newImage}
+      newLocation={newLocation}
+      setNewDescription={setNewDescription}
+      setNewImage={setNewImage}
+      setNewLocation={setNewLocation} 
+      handleFormSubmit={handleFormSubmit}
+      />
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { useState } from "react/cjs/react.development";
 import ListingCard from "./ListingCard";
 
-function ListingsContainer({listings, deleteListing}) {
+function ListingsContainer({listings, handleFormSubmit, deleteListing, checked, handleClick, newDescription, newLocation, newImage, setNewDescription, setNewLocation, setNewImage }) {
+  const [showAddListing, setShowAddListing] = useState(false)
+
+  function handleShowForm() {
+    setShowAddListing(showAddListing => !showAddListing)
+  }
   return (
     <main>
+      {showAddListing 
+      ? <form onSubmit={handleFormSubmit}>
+        <input type="text" name="description" placeholder="Description" value={newDescription} onChange={(e) => setNewDescription(e.target.value)} />
+        <input type="text" name="image" placeholder="Image URL" value={newImage} onChange={(e) => setNewImage(e.target.value)} />
+        <input type="text" name="listing" placeholder="Location" value={newLocation} onChange={(e) => setNewLocation(e.target.value)} />
+        <button type="submit" >Add listing</button>
+      </form>
+      : null
+}
+      <div>
+        <input type="checkbox" id="sort-location" name="scales" checked={checked} onClick={handleClick} />
+        <label for="sort-location">Sort by location</label>
+      </div>
+      <button onClick={handleShowForm}>Add New Listing</button>
       <ul className="cards">
         {listings.map(listing => (
           <ListingCard 
